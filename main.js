68b6453f56684bdf10d6a5ce698b9b3d3c2a8822
Palak Khan <palakkhan2002@gmail.com>
2024-06-17 18:40:35 +0530

    cursor added

--- main.js
+++ main.js
@@ -226,3 +226,8 @@ document.onkeydown = function(e) {
       return false;
   }
 };
+var cursor = document.querySelector('.cursor');
+var cursor2 = document.querySelector('.cursor2');
+document.addEventListener('mousemove',function(e){
+cursor.style.cssText = cursor2.style.cssText = "left:" + e.clientX + "px; top:" + e.clientY + "px;";
+})
